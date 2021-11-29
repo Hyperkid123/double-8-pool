71bf81bf1f00041292a0a9d34b19c7fbc39f9c9d
Martin Maroši <marvusm.mmi@gmail.com>
2021-11-29 14:26:09 +0100

    Add table as background

--- src/client/index.js
+++ src/client/index.js
@@ -5,16 +5,18 @@ function preload ()
     this.load.setBaseURL('/')
     this.objects = {};
     this.load.image('black-ball', 'assets/black-ball.png')
+    this.load.image('table', 'assets/table.png')
 }
 
 function create ()
 {
+    const table = this.add.image(1253/2, 652/2, 'table')
     const blackBall = this.physics.add.image(400, 300, 'black-ball');
     this.objects.camera = this.cameras.add(0, 0, 800, 600);
     blackBall.setVelocity(100, 200);
     blackBall.setBounce(1, 1);
     blackBall.setCollideWorldBounds(true);
-    this.objects.camera.setBackgroundColor('rgba(255, 0, 0, 0.5)');
+    // this.objects.camera.setBackgroundColor('rgba(255, 0, 0, 0.5)');
 }
 
 function update() {
@@ -23,8 +25,8 @@ function update() {
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1253,
+    height: 632,
     physics: {
         default: 'arcade',
         arcade: {

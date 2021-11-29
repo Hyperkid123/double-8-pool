60205ec7ddcdfdcbd250e175ca1bec8f0df6a168
rvsia <r.vsia@seznam.cz>
2021-11-29 16:35:07 +0100

    Add eslint

--- src/client/index.js
+++ src/client/index.js
@@ -1,31 +1,31 @@
 import * as Phaser from 'phaser';
 
 class Scene extends Phaser.Scene {
-    constructor() {
-        super()
-        this.graphics;
-        this.mouseFollow
-        this.stick
-        this.isPointerDown = false
-        this.POWER_INCREMENT = 0.1
-        this.POWER_MAXIMUM = 10
-        this.CURRENT_POWER = 0
-    }
+  constructor() {
+    super()
+    this.graphics;
+    this.mouseFollow
+    this.stick
+    this.isPointerDown = false
+    this.POWER_INCREMENT = 0.1
+    this.POWER_MAXIMUM = 10
+    this.CURRENT_POWER = 0
+  }
 
-    preload ()
-    {
-        this.load.setBaseURL('/')
-        this.objects = {};
-        this.load.spritesheet('whiteball', 'assets/whiteball.png', {frameWidth: 69, frameHeight: 68})
-        this.load.image('table', 'assets/table.png')
-        this.load.spritesheet('stick', 'assets/stick.png', {frameWidth: 11, frameHeight: 455})
-    }   
+  preload ()
+  {
+    this.load.setBaseURL('/')
+    this.objects = {};
+    this.load.spritesheet('whiteball', 'assets/whiteball.png', {frameWidth: 69, frameHeight: 68})
+    this.load.image('table', 'assets/table.png')
+    this.load.spritesheet('stick', 'assets/stick.png', {frameWidth: 11, frameHeight: 455})
+  }
 
-    create ()
-{
+  create ()
+  {
     this.graphics = this.add.graphics();
     const table = this.physics.add.staticImage(1253/2, 652/2, 'table').refreshBody()
-    this.whiteball = this.physics.add.sprite(400, 300, 'whiteball');    
+    this.whiteball = this.physics.add.sprite(400, 300, 'whiteball');
     this.stick = this.physics.add.sprite(100, 100, 'stick');
     this.stick.setOrigin(0, 0)
     this.stick.setRotation(Math.PI / 2)
@@ -33,68 +33,68 @@ class Scene extends Phaser.Scene {
     this.mouseFollow = new Phaser.Geom.Circle(0, 0, 25)
     this.objects.camera = this.cameras.add(0, 0, 800, 600);
     this.input.on('pointermove', pointer => {
-        if(!this.isPointerDown) {
-            this.mouseFollow.x = pointer.x
-            this.mouseFollow.y = pointer.y
-            const point =  Phaser.Math.RotateAroundDistance({x: this.whiteball.x, y: this.whiteball.y}, this.stick.x, this.stick.y, this.stick.angle, 50)
-            this.stick.x = pointer.x
-            this.stick.y = pointer.y
-            const angleBetween = Phaser.Math.Angle.Between(this.whiteball.x, this.whiteball.y, this.stick.x, this.stick.y)
-            console.log({angleBetween, inGed: Phaser.Math.Angle.WrapDegrees(angleBetween)})
-            this.stick.angle = Phaser.Math.RadToDeg(angleBetween + Math.PI / 2)
-        }
+      if(!this.isPointerDown) {
+        this.mouseFollow.x = pointer.x
+        this.mouseFollow.y = pointer.y
+        const point =  Phaser.Math.RotateAroundDistance({x: this.whiteball.x, y: this.whiteball.y}, this.stick.x, this.stick.y, this.stick.angle, 50)
+        this.stick.x = pointer.x
+        this.stick.y = pointer.y
+        const angleBetween = Phaser.Math.Angle.Between(this.whiteball.x, this.whiteball.y, this.stick.x, this.stick.y)
+        console.log({angleBetween, inGed: Phaser.Math.Angle.WrapDegrees(angleBetween)})
+        this.stick.angle = Phaser.Math.RadToDeg(angleBetween + Math.PI / 2)
+      }
     })
     this.input.on('wheel', (_a, _b, _c, delta) => {
-        if(delta > 0) {
-            this.stick.angle += 5
-        } else if (delta < 0) {
-            this.stick.angle -= 5
-        }
+      if(delta > 0) {
+        this.stick.angle += 5
+      } else if (delta < 0) {
+        this.stick.angle -= 5
+      }
     })
 
     this.input.on('pointerdown', () => {
-        console.log('INPUT DOWN')
-        this.isPointerDown = true
+      console.log('INPUT DOWN')
+      this.isPointerDown = true
     })
 
     this.input.on('pointerup', (pointer) => {
-        console.log('INPUT UP')
-        this.isPointerDown = false
-        this.CURRENT_POWER = 0
-        this.stick.x = pointer.x
-        this.stick.y = pointer.y
+      console.log('INPUT UP')
+      this.isPointerDown = false
+      this.CURRENT_POWER = 0
+      this.stick.x = pointer.x
+      this.stick.y = pointer.y
     })
-}
+  }
 
-update() {
+  update() {
     this.graphics.clear();
     if(this.isPointerDown && this.CURRENT_POWER <= this.POWER_MAXIMUM) {
-        this.CURRENT_POWER += this.POWER_INCREMENT
-        this.stick.x += this.POWER_INCREMENT * 3
-        this.stick.y += this.POWER_INCREMENT * 3
+      this.CURRENT_POWER += this.POWER_INCREMENT
+      this.stick.x += this.POWER_INCREMENT * 3
+      this.stick.y += this.POWER_INCREMENT * 3
     }
 
     this.graphics.lineStyle(1, 0x5391c9, 1);
 
-    this.graphics.strokeCircleShape(this.mouseFollow);    
-    }
+    this.graphics.strokeCircleShape(this.mouseFollow);
+  }
 }
 
 
 const config = {
-    type: Phaser.AUTO,
-    width: 1253,
-    height: 632,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                y: 0,
-                x: 0
-            }
-        }
-    },
-    scene: [ Scene ]
+  type: Phaser.AUTO,
+  width: 1253,
+  height: 632,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 0,
+        x: 0
+      }
+    }
+  },
+  scene: [ Scene ]
 }
 
 const game = new Phaser.Game(config);
